c0cb5017e2dd4498a43a421090f417426cc7fc6c
shun <quxiangshun@gmail.com>
2022-05-10 15:08:34 +0800

    feat: 配置端口号、头部、

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,18 +1,18 @@
 export default {
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'tyxq-blog',
+    title: '天韵戏曲博客',
     htmlAttrs: {
       lang: 'en'
     },
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' },
+      { hid: 'description', name: 'description', content: '黄梅戏，京剧，沪剧，昆剧，其他剧种' },
       { name: 'format-detection', content: 'telephone=no' }
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+      { rel: 'icon', type: 'image/x-icon', href: '/title.png' }
     ]
   },
 
