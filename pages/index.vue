c1d70b3c20aa76973865fa8bfe48622e59043c32
shun <quxiangshun@gmail.com>
2022-05-15 13:43:11 +0800

    feat: 样式

--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,14 @@
 <template>
   <div>
-    <el-button type="primary">aaaa</el-button>
+    <el-button v-for="(item, index) in 100" :key="index" type="primary">aaaa</el-button>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'IndexPage'
+  name: 'IndexPage',
+  asyncData() {
+    console.log('process.env.authURL', process.env.authURL)
+  }
 }
 </script>
